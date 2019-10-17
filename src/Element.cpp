23a424768849cc048d788785cc75faf594b465bf
Gregory Shtrasberg <gregory.shtrasberg@verizondigitalmedia.com>
2019-10-17 14:37:38 -0400

    Initial commit

---
+++ src/Element.cpp
@@ -0,0 +1,9 @@
+#include "Element.h"
+
+namespace BraceExp
+{
+bool Element::Parse(std::string s)
+{
+    return true;
+}
+} // namespace BraceExp
---
+++ src/Element.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+
+namespace BraceExp
+{
+class Element
+{
+public:
+    bool Parse(std::string s);
+
+private:
+};
+} // namespace BraceExp
