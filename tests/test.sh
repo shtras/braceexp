a4b8908a27c53313b71b766da5154053fbe28a8b
Gregory Shtrasberg <shtras@gmail.com>
2019-10-18 20:11:08 -0400

    Couple more tests

--- tests/test.sh
+++ tests/test.sh
@@ -31,6 +31,7 @@ run_test "{{{{{Z,Y}W,V}U,T}S,R}Q,P}{A,B{C,D{E,F{G,H{I,J{K,L{M,N{O,P{Q,R{S,T{U,V{
 run_test "{A,B}C{D,E}" {A,B}C{D,E}
 run_test "{AB,CD}EF{GH,JK}" {AB,CD}EF{GH,JK}
 run_test "{A,B}{C,D}{E,F}{G,H}" {A,B}{C,D}{E,F}{G,H}
+run_test "JK{AB{EEE,FQ}GGG{A{B,CDR{GT,F}R{QW,AS}T,GHJ}FF,LL},TY}" JK{AB{EEE,FQ}GGG{A{B,CDR{GT,F}R{QW,AS}T,GHJ}FF,LL},TY}
 
 run_test "}ABC"
 run_test "{ABC"
@@ -39,5 +40,8 @@ run_test "{}"
 run_test "A,B,C"
 run_test "{A,}"
 run_test "A B"
+run_test "A.B"
+run_test " AB"
+run_test "AB "
 
 echo "$n tests succeeded"
